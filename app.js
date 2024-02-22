c1106564407972c20261d536d6eca123d9f0f0f2
sophie1629 <sophielauwe@gmail.com>
2024-02-22 15:36:25 -0300

    modificamos rutas en el archivo app.js

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ app.listen(PORT, () => {
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get('/login', (req,res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
