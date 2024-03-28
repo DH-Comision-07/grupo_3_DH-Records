876fdc4983c0e82033f97697264d51b625b3ce9d
floscocco <floscocco@hotmail.com>
2024-03-28 16:28:12 -0300

    feature: Agrega uso de multer en ruta y formulario

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-
+const multer = require('multer');
 const productsControllers = require('../controllers/productsControllers');
 
 
