b528d498cd525ad217784aedb8958c182f1d3722
floscocco <floscocco@hotmail.com>
2024-03-28 18:25:28 -0300

    feature:Se agrega logica para agregar id, fix: Nombres de campos en vista

--- src/models/productService.js
+++ src/models/productService.js
@@ -13,6 +13,14 @@ let productService = {
         return this.products.find(product => product.id == id);
     },
     save: function(product) {
+        let lastId = 0;
+        for (let existingProduct of this.products) {
+            if (existingProduct.id > lastId) {
+                lastId = existingProduct.id;
+            }
+        }
+        product.id = lastId + 1;
+        
         this.products.push(product);
     }
 }
