083d0091780732a5cd03e6bb476460a948b1cca5
Mariano Xinos <mxinos@hotmail.com>
2024-04-02 22:18:05 -0300

    Temrine users/login con sus validaciones de error

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,5 +1,6 @@
-const userService = require('../models/usersService');
+const usersService = require('../models/usersService');
 const { validationResult } = require('express-validator');
+
 
 let usersControllers = {
 
@@ -17,22 +18,29 @@ let usersControllers = {
         };
     },
 
+
     login: function (req, res) {
-        res.render('users/login');
+        res.render('users/login',{ errores: [], oldData: {} });
     },
 
     processLogin: function (req, res) {
-       const errores=  validationResult(req);
+       const errors=  validationResult(req);
+
+       if(!errors.isEmpty()){
+        res.render('users/login', { errores: errors.mapped(), oldData: req.body });
+       } else{
+            return res.redirect('/users/detail/:id')
+       }
     },
     
 
     detail: function (req, res) {
-        let productId = (usersService.getBy(req.params.id));
+        let userId = (usersService.getBy(req.params.id));
         res.render('users/detail', {userId});
     },
 
     getAll: function (req, res) {
-        res.render('users', {users: userService.getAll()});
+        res.render('users', {users: usersService.getAll()});
 
     }
 };
--- src/middlewares/loginMid.js
+++ src/middlewares/loginMid.js
@@ -1,8 +1,8 @@
 const { body } = require('express-validator');
 
 const validacionesLogin= [
-    body('usuario').notEmpty().withMessage('Debes completar el campo usuario'),
-    body('contraseña').notEmpty().withMessage('Debes completar el campo contraseña')
+    body('usuario').notEmpty().withMessage('You must complete the user field'),
+    body('contraseña').notEmpty().withMessage('You must complete the password field')
 ];
 
 module.exports= validacionesLogin;
