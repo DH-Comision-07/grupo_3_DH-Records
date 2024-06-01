bb1b75c843262112e5393928e0d0596fd8a78459
Mariano Xinos <mxinos@hotmail.com>
2024-06-01 16:04:25 -0300

    Add: Asosiacion entre la tabla Users y Categoria

--- src/models/db/models/categoria.js
+++ src/models/db/models/categoria.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      categoria.hasMany(models.Users,{
+        as:'users',
+        foreignKey:'categoriaId'
+      });
     }
   }
   categoria.init({
--- src/models/db/models/users.js
+++ src/models/db/models/users.js
@@ -1,24 +1,27 @@
 'use strict';
 const { Model } = require('sequelize');
+const categoria = require('./categoria');
 
 module.exports = (sequelize, DataTypes) => {
 class Users extends Model {
-  /* 
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
-      this.belongsToMany(models.Products,{
-        as: 'products',
-        through: 'users_products',
-        foreignKey: 'usersId'
-      });
-      
+      Users.belongsTo(models.categoria, {
+        as: 'categoria',
+        foreignKey: 'categoriaId'
+      })
     }
-    */
   }
   Users.init({
     nombreUsuario: DataTypes.STRING,
     email: DataTypes.STRING,
     contraseña: DataTypes.STRING,
-    terminosCondiciones: DataTypes.BOOLEAN
+    terminosCondiciones: DataTypes.BOOLEAN,
+    categoriaId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Users',
