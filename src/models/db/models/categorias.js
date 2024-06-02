6f5e7eabb2437aeae8f5fcc01412c3e8e07cfdc8
Mariano Xinos <mxinos@hotmail.com>
2024-06-02 17:46:54 -0300

    Add: Tabla Secundaria Categorias, migration + model + asociacion con la tabla users

---
+++ src/models/db/models/categorias.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class categorias extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.Users, {
+        as: 'users',
+        foreignKey: 'categoriasId'
+      });
+    }
+  }
+  categorias.init({
+    admin: DataTypes.STRING,
+    usuario: DataTypes.STRING
+  }, {
+    sequelize,
+    timestamps:false,
+    modelName: 'categorias',
+  });
+  return categorias;
+};
