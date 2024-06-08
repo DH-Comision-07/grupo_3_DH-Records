8c55a266adce154ca4d3ea032f53f88078107cda
MarianoXinos1 <xinosmariano@gmail.com>
2024-06-08 16:47:23 -0300

    Add: Validacion del frontend de la contraseña en el login

--- public/js/users/loginValidation.js
+++ public/js/users/loginValidation.js
@@ -2,26 +2,36 @@ window.onload = function() {
 
     let formLogin = document.querySelectorAll('input');
 
-    let inputField = document.querySelector('#email');
-     inputField.focus();
+    let emailField = document.querySelector('#email');
+     emailField.focus();
 
-     
-let errorMessages = document.querySelectorAll('.error-message');
+     let passwordField = document.querySelector('#contraseña');
+
+    let errorMessagesEmail = document.querySelectorAll('.error__message--email');
+
+    let errorMessagesPassword = document.querySelector('.error__message--password');
 
         
    formLogin.forEach((formField,index) => {
+
         formField.addEventListener('input', function(event){
             if(formField.value === '') {
                 formField.classList.add('is-invalid');
-                errorMessages[index].textContent = 'The email address is required';
+                if(formField === emailField){
+                    errorMessagesEmail[index].textContent = 'The email address is required';
+                } else if (formField === passwordField) {
+                    errorMessagesPassword.textContent = 'The password is required';
+                }
+
             } else {
                 formField.classList.remove('is-invalid');
-                errorMessages[index].textContent = '';
-            }
+                if(formField === emailField){
+                    errorMessagesEmail[index].textContent = '';
+                } else if (formField === passwordField) {
+                    errorMessagesPassword.textContent = '';
+                }
+            } 
         })
-
-
-
 
     });
 }
