70a1bdb37b5f22a5ed2a74424fc9995d5b947939
MarianoXinos1 <xinosmariano@gmail.com>
2024-06-30 14:26:02 -0300

    Fix: Se arreglo un error el mapeo para el Sedeers del Users

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const path = require ('path');
+const userLoggedMid = require('./middlewares/userLoggedMid');
+
+const app = express();
+
+app.use(userLoggedMid);
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+
+
+const PORT = 3030; 
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/login', (req,res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+
+app.get('/ediciondeproductos', (req,res) => {
+  res.sendFile(path.resolve(__dirname, './views/ediciondeproductos.html'));
+});
--- src/models/db/migrations/20240505205710-create-users.js
+++ src/models/db/migrations/20240505205710-create-users.js
@@ -29,7 +29,7 @@ module.exports = {
       }
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {  
     await queryInterface.dropTable('Users');
   }
 };
