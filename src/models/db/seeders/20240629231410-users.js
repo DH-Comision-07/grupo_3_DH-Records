c8f3773897507f51716386e59505274bd4395570
MarianoXinos1 <xinosmariano@gmail.com>
2024-07-01 12:36:35 -0300

    Add: Seeder users se le agrego la encriptacion de la contraseña

--- src/models/db/seeders/20240629231410-users.js
+++ src/models/db/seeders/20240629231410-users.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const bcryptjs = require('bcryptjs');
+
 /** @type {import('sequelize-cli').Migration} */
 
 function Users(nombreUsuario,email,contraseña,imagenUsuario,terminosCondiciones,categorias_id){
   this.nombreUsuario = nombreUsuario;
   this.email = email;
-  this.contraseña = contraseña;
+  this.contraseña = bcryptjs.hashSync(contraseña,10);
   this.imagenUsuario = imagenUsuario;
   this.terminosCondiciones = terminosCondiciones;
   this.categorias_id = categorias_id;
@@ -15,12 +17,12 @@ module.exports = {
   async up (queryInterface, Sequelize) {
   
     let users = [
-      new Users('Mariano','marian@hotmail.com',12345678,'userProfile-1718577395048.jpg',0,1),
-      new Users('Dani','dani@hotmail.com',12345678,'userProfile-1718637185571.jpg',0,2),
-      new Users('Nico','nico@hotmail.com',12345678,'userProfile-1718577395048.jpg',0,2),
-      new Users('Juan','juan@hotmail.com',12345678,'userProfile-1718637185571.jpg',0,2),
-      new Users('Lucas','lucas@hotmail.com',12345678,'userProfile-1718577395048.jpg',0,2),
-      new Users('Jaime','jaime@hotmail.com',12345678,'userProfile-1718637185571.jpg',0,2)
+      new Users('Mariano','marian@hotmail.com','12345678','userProfile-1718577395048.jpg',0,1),
+      new Users('Dani','dani@hotmail.com','12345678','userProfile-1718637185571.jpg',0,2),
+      new Users('Nico','nico@hotmail.com','12345678','userProfile-1718577395048.jpg',0,2),
+      new Users('Juan','juan@hotmail.com','12345678','userProfile-1718637185571.jpg',0,2),
+      new Users('Lucas','lucas@hotmail.com','12345678','userProfile-1718577395048.jpg',0,2),
+      new Users('Jaime','jaime@hotmail.com','12345678','userProfile-1718637185571.jpg',0,2)
     ]
 
 
