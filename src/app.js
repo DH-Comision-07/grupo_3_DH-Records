24456a1503b8bcd9248a589d0affcfa0867ff54d
MarianoXinos1 <xinosmariano@gmail.com>
2024-07-05 11:31:29 -0300

    Fix: Apis

--- src/app.js
+++ src/app.js
@@ -41,6 +41,12 @@ app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
 //Confir de rutas
 const homeRoutes= require('./routes/home.routes');
-app.use('/', homeRoutes);
+const productsRoutes = require('./routes/products.routes');
+const usersRoutes = require('./routes/users.routes');
+const apisRoutes = require('./routes/apis.routes');
 
- 
+
+app.use('/', homeRoutes);
+app.use('/products', productsRoutes);
+app.use('/users', usersRoutes);
+app.use('/apis', apisRoutes);
